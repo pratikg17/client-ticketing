5db417a6b2d1481109e7cb406ac14b2ddf533470
pratikg17 <pjgawand@gmail.com>
2021-04-07 18:13:05 +0530

    Auth Sign up

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -2,6 +2,13 @@ export default () => {
   return (
     <form>
       <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input className="form-control"></input>
+        <label>Password</label>
+        <input className="form-control" type="password"></input>
+        <button className="btn btn-primary">Sign Up</button>
+      </div>
     </form>
   );
 };
