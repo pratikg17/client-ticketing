3027d154d9b888fd8ba52d2b4ad337c20fc72ee9
pratikg17 <pjgawand@gmail.com>
2021-04-09 11:02:39 +0530

    Using axios.create

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,14 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log('I am in the component', currentUser);
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  console.log(req.headers);
-  if (typeof window === 'undefined') {
-    // We are on the server
-    // request should be made to http://ingress-nginx.ingress
-    const { data } = await axios.get(
-      // 'http://SERVICENAME.NAMESPACE.svc.cluster.local'
-      'http://ingress-nginx-controller.kube-system.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
-    return data;
-  } else {
-    // We are on the browser
-    // request can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
-  return {};
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
 };
 
 export default LandingPage;
