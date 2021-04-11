42087400563f5219dc27a31a5a62e06f7563ccdb
pratikg17 <pjgawand@gmail.com>
2021-04-11 19:45:38 +0530

    Header

--- components/header.js
+++ components/header.js
@@ -1,18 +1,17 @@
-import { Link } from 'next/link';
+import Link from 'next/link';
 
-const header = ({ currentUser }) => {
+export default ({ currentUser }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href="/">
         <a className="navbar-brand">GitTix</a>
       </Link>
+
       <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items items-center">
+        <ul className="nav d-flex align-items-center">
           {currentUser ? 'Sign out' : 'Sign in/up'}
         </ul>
       </div>
     </nav>
   );
 };
-
-export default header;
