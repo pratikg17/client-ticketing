d175110f1d177de30b92d9313a0a4869254f04ed
pratikg17 <pjgawand@gmail.com>
2021-04-11 14:45:59 +0530

    Issue fix

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,17 @@ import buildClient from '../api/build-client';
 const AppComponent = ({ Component, pageProps }) => {
   return (
     <div>
-      <h1>Header</h1>
+      <h1>Header!</h1>
       <Component {...pageProps} />
     </div>
   );
 };
 
-AppComponent.getIntitialProps = async (appContext) => {
-  console.log(Object.keys(appContext));
-  console.log('LANDING PAGE', appContext);
+AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx);
   const { data } = await client.get('/api/users/currentuser');
-  const pageProps = await appContext.Component.getIntitialProps(appContext.ctx);
-  console.log('pageProps', pageProps);
+
   return data;
 };
+
 export default AppComponent;
