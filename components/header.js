ebe362e7874e10434c3fe5a5ecfec986bc069187
pratikg17 <pjgawand@gmail.com>
2021-04-11 19:53:09 +0530

    Header

--- components/header.js
+++ components/header.js
@@ -1,6 +1,31 @@
 import Link from 'next/link';
 
 export default ({ currentUser }) => {
+  const links = [
+    !currentUser && {
+      label: 'Sign Up',
+      href: '/auth/signup',
+    },
+    !currentUser && {
+      label: 'Sign In',
+      href: '/auth/signin',
+    },
+    currentUser && {
+      label: 'Sign Out',
+      href: '/auth/signout',
+    },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className="nav-item">
+          <Link href={href}>
+            <a className="navbar-brand">{label}</a>
+          </Link>
+        </li>
+      );
+    });
+
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href="/">
@@ -8,9 +33,7 @@ export default ({ currentUser }) => {
       </Link>
 
       <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">
-          {currentUser ? 'Sign out' : 'Sign in/up'}
-        </ul>
+        <ul className="nav d-flex align-items-center">{links} </ul>
       </div>
     </nav>
   );
