5ba86c944ce85e447bfbd8421c709d10e498e28c
pratikg17 <pjgawand@gmail.com>
2021-04-11 14:41:16 +0530

    Chnages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h1>Header</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getIntitialProps = async (appContext) => {
+  console.log(Object.keys(appContext));
+  console.log('LANDING PAGE', appContext);
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+  const pageProps = await appContext.Component.getIntitialProps(appContext.ctx);
+  console.log('pageProps', pageProps);
+  return data;
+};
+export default AppComponent;
